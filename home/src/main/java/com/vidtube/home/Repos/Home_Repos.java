62120e5453c4a2428fa286cfdf1ca8f91fbbda5f
deki08 <105973954+deki08@users.noreply.github.com>
2022-12-28 22:10:40 +0530

    Delete home directory

--- home/src/main/java/com/vidtube/home/Repos/Home_Repos.java
+++
@@ -1,9 +0,0 @@
-package com.vidtube.home.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vidtube.home.Entity.Home_Entity;
-
-public interface Home_Repos extends JpaRepository<Home_Entity,String> {
-
-}
